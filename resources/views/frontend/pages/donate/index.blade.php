78c3e7c49ada6dac39431aa30850bb9d4b966155
Leo <hello@leojr.me>
2019-12-30 23:12:56 -0500

    /

--- resources/views/frontend/pages/donate/index.blade.php
+++ resources/views/frontend/pages/donate/index.blade.php
@@ -2,5 +2,14 @@
 @section('title', 'Wonderful Works Of Jesus')
 @section('meta-description', 'Jesus Lives Here')
 @section('content')
+    <section class="container">
+        <div class="col-md-9">
+            <h2>Help Protect Christian Rights</h2>
+            <p>Wonderful Works Of Jesus is a non-profit organization, not a registered charity. Our work focuses on complex issues of Christian Persecution, rights of Christians. By giving to Wonderful Works of Jesus, you’re helping to build systemic change that will empower and improve the lives of Christians across Canada, the world, and defend our human rights.</p>
+        </div>
 
+        <div class="col-md-3">
+            
+        </div>
+    </section>
 @endsection
--- resources/views/frontend/pages/persecution_of_christians/index.blade.php
+++ resources/views/frontend/pages/persecution_of_christians/index.blade.php
@@ -2,5 +2,30 @@
 @section('title', 'Wonderful Works Of Jesus')
 @section('meta-description', 'Jesus Lives Here')
 @section('content')
+    <section class="container">
+        <p>Below, we share a quick glance at the numbers during the 2019 reporting period (from Oct. 31, 2017, to November 1, 2018). But by no means do the numbers tell the full story. Each fact and figure represent real people with real lives, families, jobs, churches—fathers, mothers, husbands, wives, sons, daughters—who every day make the dangerous choice to believe in and trust Jesus.</p>
+    </section>
 
+    <section class="container">
+        <div>
+            </p><h3>In a Year</h3>
+            <p><strong>245 Million:</strong> In the top 50 World Watch List countries alone, 245 million Christians in the world experience high levels of persecution for their choice to follow Christ.</p>
+            <p><strong>1 in 9:</strong> Christians worldwide experience high levels of persecution</p>
+            <p><strong>14%:</strong> The rise in the number of Christians in the top 50 countries on the 2019 World Watch List (WWL) who experience high levels of persecution. (from the 2018 reporting period to 2019’s)</p>
+            <p><strong>4,136:</strong> Christians killed for faith-related reasons in the top 50 WWL countries.</p>
+            <p><strong>2,625:</strong> Christians detained without trial, arrested, sentenced and imprisoned in the top 50 WWL countries.</p>
+            <p><strong>1,266:</strong> churches or Christian buildings attacked in the top 50 WWL countries.</p>
+            <p><strong>7 out of 9:</strong> In seven of the countries in the World Watch List’s top 10, the primary cause of persecution is Islamic oppression.</p>
+            <p><strong>11:</strong> countries scoring in the “extreme” level for their persecution of Christians. Five years ago, North Korea was the only one.</p>
+            <p><strong>18:</strong> Consecutive years North Korea has ranked No. 1 as the world’s most dangerous place for Christians.</p>
+            <h3>Every Month</h3>
+            <p><strong>105:</strong> churches attacked, burned or vandalized each month, in the top 50 WWL countries</p>
+            <h3>Every Day</h3>
+            <p><strong>11:</strong> Christians killed every day for their faith, in the top 50 WWL countries.</p>
+            <h3>By the Continent</h3>
+            <p><strong>1 out of 6:</strong> Christians in Africa that experience high levels of persecution.</p>
+            <p><strong>1 out of 3:</strong> Christians in Asia that experience high levels of persecution.</p>
+            <p><strong>1 out of 21:</strong> Christians in South America who experience high levels of persecution.</p>
+        </div>
+    </section>
 @endsection
