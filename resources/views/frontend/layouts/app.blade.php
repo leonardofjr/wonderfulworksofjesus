9ff57c69d0f24938f72cc37918dbe94aebb01ffd
Leo <hello@leojr.me>
2020-01-01 15:21:29 -0500

    /

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -51,13 +51,14 @@
                 @yield('content')
             </section>
         </main>
-        <footer>
-            @if (!Request::is('/'))
-                <div class="footer-info text-center">
-                    <small> Copyright &copy; 2018 Wonderful Works Of Jesus</small>
-                </div>
-            @endif
-        </footer>
+        @if (!Request::is('/'))
+            <footer>
+                    <div class="footer-info text-center">
+                        <small> Copyright &copy; 2018 Wonderful Works Of Jesus</small>
+                    </div>
+            </footer>
+        @endif
+
     </body>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
